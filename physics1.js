6d3204f64477d5f3504514e41eeb87a61835fa3f
Josiah Hunsinger <fencepost13302@gmail.com>
2025-01-31 12:38:45 +0700

    Collisions being detected and corrected at least as far as displacement goes. About to impliment rebound logic

--- physics1.js
+++ physics1.js
@@ -4,20 +4,28 @@ Algebra(2,0,1,()=>{
   let dist_pl = (p, l) => (p^l).e012;
   let wrapped_pairs = a => a.map((x, i)=>[x, a[(i+1)%a.length]]);
   let check_wall_collision = (obj, walls) => {
-    // If there is a collision return its normal line. Otherwise return null.
+    // If there is a collision, correct it and return its normal line. Otherwise return null.
     let world_points = obj.M >>> obj.points;
+    let worst_P=null, worst_wall=null, worst_d=0;
     for (let p=0; p<world_points.length; p++) {
       let P = world_points[p];
       for (let w=0; w<walls.length; w++) {
         let wall = walls[w];
         let d = dist_pl(P, wall);
-        if (d < 0) {
-          let n = (wall | 1e12) / 1e12;
-          return n | P;
+        if (d < worst_d) {
+          worst_P = P; worst_wall = wall; worst_d = d;
         }
       }
     }
 
+    if (worst_d < 0) {
+      let n = -(worst_wall | 1e12) / 1e12;
+      obj.M = (1-0.5*worst_d*1e0*n)*obj.M;
+      return n | worst_P;
+    }
+  }
+  let rebound = (N, obj1, obj2) => {
+    // N should be oriented in the direction which obj1 should rebound
   }
   
   let box_points = [
@@ -26,8 +34,11 @@ Algebra(2,0,1,()=>{
     point(-0.5,-0.5),
     point( 0.5,-0.5)
   ];
+
   let objects = [{
     points: box_points,
+    I: x => x.Dual,
+    Iinv: x => x.UnDual,
     M: 1 + 0.5*1e02,
     B: 1e02 + 1e12,
     color: 0x1111aa
@@ -62,7 +73,7 @@ Algebra(2,0,1,()=>{
 
     return [
       // Objects
-      ...objects.map(o=>[o.color, o.M>>>o.points]).flat(),
+      ...objects.map(o=>[check_wall_collision(o, walls) ? 0x992222 : o.color, o.M>>>o.points]).flat(),
 
       // Walls
       0xaa1111, ...walls,
